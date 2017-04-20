8f28789e221b28640bbee0a31aafb4de17f0b48a
stanaka <stanaka@fourmix.co.jp>
2017-04-20 11:48:36 +0900

    モック　反映

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,97 @@
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
+
+/*
+|--------------------------------------------------------------------------
+| モックルート
+|--------------------------------------------------------------------------
+*/
+
+/*
+|--------------------------------------------------------------------------
+| 作業日報
+|	入力、閲覧、集計
+|--------------------------------------------------------------------------
+*/
+Route::get('/daily/', function () {
+    return view('layouts.content.daily.index');
+});
+Route::get('/daily/', function () {
+    return view('layouts.content.daily.index');
+});
+Route::get('/daily/', function () {
+    return view('layouts.content.daily.index');
+});
+
+/*
+|--------------------------------------------------------------------------
+| プロジェクト
+|	一覧、個人予算、台帳、予算対
+|--------------------------------------------------------------------------
+*/
+// 一覧
+Route::get('/project/', function () {
+    return view('layouts.content.project.category.index');
+});
+// 個人予算
+Route::get('/project/personal-budget', function () {
+    return view('layouts.content.project.category.personal-budget');
+});
+// 台帳
+Route::get('/project/ledger', function () {
+    return view('layouts.content.project.category.ledger');
+});
+// 予算対
+Route::get('/project/project-budget', function () {
+    return view('layouts.content.project.category.project-budget');
+});
+
+/*
+|--------------------------------------------------------------------------
+| 基本設定
+|	担当者、作業分類、部門分類、勤務分類、得意先分類
+|--------------------------------------------------------------------------
+*/
+Route::get('/setting/staff', function () {
+    return view('layouts.content.setting.category.staff');
+});
+Route::get('/setting/work', function () {
+    return view('layouts.content.setting.category.work');
+});
+Route::get('/setting/departments', function () {
+    return view('layouts.content.setting.category.departments');
+});
+Route::get('/setting/service', function () {
+    return view('layouts.content.setting.category.service');
+});
+Route::get('/setting/customers', function () {
+    return view('layouts.content.setting.category.customers');
+});
+
+/*
+|--------------------------------------------------------------------------
+| プロフィール情報
+|--------------------------------------------------------------------------
+*/
+Route::get('/config/', function () {
+    return view('layouts.content.config.index');
+});
+
+/*
+|--------------------------------------------------------------------------
+| 管理者設定
+|--------------------------------------------------------------------------
+*/
+// Route::get('/config/', function () {
+//     return view('layouts.content.config.index');
+// });
