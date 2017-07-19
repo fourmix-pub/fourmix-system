4bf288c7da7545406556b27a4d85fb14231d0e85
kgo <kgo@fourmix.co.jp>
2017-07-19 10:56:04 +0900

    サービス

--- resources/views/config/index.blade.php
+++ resources/views/config/index.blade.php
@@ -22,7 +22,7 @@
 <div class="row">
 	<div class="col-xs-12 col-sm-12 col-md-9 col-lg-9">
 
-		{{-- PC版サイドメニュー --}}
+		{{-- SP 版サイドメニュー --}}
 		<div class="row">
 			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 				@include('layouts.content.config.xs-side-menu')
@@ -36,59 +36,60 @@
 
 					<div class="panel-body">
 						<div class="row">
-							<div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+							<div class="col-xs-12 col-sm-12 col-md-8 col-lg-8">
 
-								<form>
-								@component('components.elements.form.user', ['name' => 'name'])
-									ユーザー名
-								@endcomponent
-								@component('components.elements.form.user', ['name' => 'mail'])
-									メールアドレス
-								@endcomponent
-								</form>
+									@component('components.elements.form.user', ['name' => 'name'])
+										ユーザー名
+									@endcomponent
+									@component('components.elements.form.user', ['name' => 'mail'])
+										メールアドレス
+									@endcomponent
+									<div class="row">
+										<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+											<div class="form-group">
+												<h3><small>始業時刻</small></h3>
+												<div class='input-group time'>
+													<input type='text' class="form-control" value="09:30" />
+													<span class="input-group-addon">
+												<span class="glyphicon glyphicon-time"></span>
+											</span>
+												</div>
+											</div>
+										</div>
+									</div>
+									<div class="row">
+										<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+											<div class="form-group">
+												<h3><small>終業時刻</small></h3>
+												<div class='input-group time'>
+													<input type='text' class="form-control" value="18:30" />
+													<span class="input-group-addon">
+												<span class="glyphicon glyphicon-time"></span>
+											</span>
+												</div>
+											</div>
+										</div>
+									</div>
 
 							</div>
-							<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+							<div class="col-xs-0 col-sm-0 col-md-4 col-lg-4">
+							</div>
+
+						</div>
+						<div class="row">
+							<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+								<div class="text-right">
+									<button type="submit" class="btn btn-primary">登録</button>
+								</div>
 							</div>
 						</div>
-					</div>
-						<form>
-							<div class="row">
-								<div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
-									<div class="form-group">
-										<h3><small>始業時刻</small></h3>
-										<div class='input-group time'>
-											<input type='text' class="form-control" value="09:30" />
-											<span class="input-group-addon">
-												<span class="glyphicon glyphicon-time"></span>
-											</span>
-										</div>
-									</div>
-								</div>
-								<div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
-									<div class="form-group">
-										<h3><small>終業時刻</small></h3>
-										<div class='input-group time'>
-											<input type='text' class="form-control" value="18:30" />
-											<span class="input-group-addon">
-												<span class="glyphicon glyphicon-time"></span>
-											</span>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div class="text-right">
-								<button type="submit" class="btn btn-primary">登録</button>
-							</div>
-						</form>
 					</div>
 				</div>
 			</div>
 		</div>
-
 	</div>
 
-	{{-- スマホ版サイドメニュー --}}
+	{{-- PC版サイドメニュー --}}
 	<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
 		@include('layouts.content.config.side-menu')
 	</div>
