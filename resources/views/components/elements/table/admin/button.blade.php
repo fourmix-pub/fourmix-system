be59d4a40848e52924ad735cb1a767a93729ed78
yfuseki <yfuseki@fourmix.co.jp>
2017-07-21 18:12:25 +0900

    色々変更

--- resources/views/components/elements/table/admin/button.blade.php
+++ resources/views/components/elements/table/admin/button.blade.php
@@ -1,9 +1,9 @@
 <td align="center">
-    <button type="button" class="btn btn-xs btn-info" data-toggle="modal" data-target="#update" title="編集">
+    <button type="button" class="btn btn-xs btn-info" data-toggle="modal" data-target="#update" title="編集" style="margin: 4px">
         <span class="glyphicon glyphicon-edit" aria-hidden="true"></span>
     </button>
-    &nbsp;
-    <button type="button" class="btn btn-xs btn-danger" data-toggle="modal" data-target="#delete" title="削除">
+    {{--&nbsp;--}}
+    <button type="button" class="btn btn-xs btn-danger" data-toggle="modal" data-target="#delete" title="削除" style="margin: 4px">
         <span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
     </button>
 </td>
