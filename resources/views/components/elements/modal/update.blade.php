db102e9dec987b4f6112367623f6f32730f12a83
yfuseki <yfuseki@fourmix.co.jp>
2017-07-27 15:40:46 +0900

    VIEW修正

--- resources/views/components/elements/modal/update.blade.php
+++ resources/views/components/elements/modal/update.blade.php
@@ -1,7 +1,7 @@
 <div class="modal fade" id="update" tabindex="-1">
     <div class="modal-dialog">
         <div class="modal-content">
-            <div class="modal-header" style="margin:2% 0%; ">
+            <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
                 <h4 class="modal-title">{{ $title }}</h4>
             </div>
