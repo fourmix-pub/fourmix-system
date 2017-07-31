594d4384d700008c5c389042327925df3336417e
yfuseki <yfuseki@fourmix.co.jp>
2017-07-31 17:21:07 +0900

    作業分類 table 作成、作業分類 Seeder 作成

--- database/migrations/2017_07_31_074957_create_work_types_table.php
+++ database/migrations/2017_07_31_074957_create_work_types_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCategoriesTable extends Migration
+class CreateWorkTypesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,10 @@ class CreateCategoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('work_types', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('category_name', 32);
+            $table->string('name', 100);
             $table->timestamps();
-            $table->softDeletes();
         });
     }
 
@@ -28,6 +27,6 @@ class CreateCategoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('work_types');
     }
 }
---
+++ database/seeds/WorkTypeSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class WorkTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('work_types')->delete();
+
+        for ($i = 0; $i < 20; $i++){
+            \App\Models\WorkType::create([
+                'name' => '作業'.$i,
+            ]);
+        }
+    }
+}
