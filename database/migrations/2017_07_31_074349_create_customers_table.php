8c8a45592a5d75eba2c1be5c7a2f12babce2dbd0
ayakasatou <asatou@fourmix.co.jp>
2017-07-31 17:22:29 +0900

    顧客table,seeder

--- database/migrations/2017_07_31_074349_create_customers_table.php
+++ database/migrations/2017_07_31_074349_create_customers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCategoriesTable extends Migration
+class CreateCustomersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,11 @@ class CreateCategoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('customers', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('category_name', 32);
+            $table->string('name', 100);
+            $table->integer('type_id');
             $table->timestamps();
-            $table->softDeletes();
         });
     }
 
@@ -28,6 +28,6 @@ class CreateCategoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('customers');
     }
 }
