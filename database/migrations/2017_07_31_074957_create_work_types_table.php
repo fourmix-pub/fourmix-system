d97a615ea910506d5a50a3a00bb71f74f29562a5
yfuseki <yfuseki@fourmix.co.jp>
2017-08-02 10:56:55 +0900

    レビュー箇所修正　論理削除追加

--- database/migrations/2017_07_31_074957_create_work_types_table.php
+++ database/migrations/2017_07_31_074957_create_work_types_table.php
@@ -17,6 +17,7 @@ class CreateWorkTypesTable extends Migration
             $table->increments('id');
             $table->string('name', 100);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
