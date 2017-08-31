85394496d8edc1987727d4113e8a6308d3c168c7
kgo_fourmix <kgo@fourmix.co.jp>
2017-08-31 12:27:03 +0900

    menu config 追加

---
+++ config/menu.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Menu
+    |--------------------------------------------------------------------------
+    */
+
+    'tools' => [
+        'phpMyAdmin' => env('PHP_MY_ADMIN', '#'),
+        'git' => env('GIT', 'https://www2.fourmix.co.jp'),
+        'redmine' => env('REDMINE', 'https://redmine.fourmix.co.jp'),
+        'sweeper' => env('SWEEPER', 'http://172.16.14.80/portal/portal.php'),
+    ],
+
+];
