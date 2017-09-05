8ce56fe23489e65205fc9e8b877a75ab47fdea0a
yfuseki <yfuseki@fourmix.co.jp>
2017-09-05 17:12:39 +0900

    CI変更

---
+++ tests/Browser/LoginTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Browser;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class LoginTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                ->assertSee('E-MAIL')
+                ->assertSee('PASSWORD')
+                ->assertSee('ブラウザに保存')
+                ->assertSee('LOGIN')
+                ->assertSee('パスワードを忘れた方')
+                ->clickLink('パスワードを忘れた方')
+                ->assertSee('PASSWORD RESET')
+                ->assertSee('E-MAIL')
+                ->assertSee('メール送信')
+                ->visit('/')
+                ->type('email', 'fourmix-system@fourmix.co.jp')
+                ->type('password', '123456')
+                ->press('LOGIN')
+                ->assertPathIs('/dailies');
+        });
+    }
+}
