107e2ae81df9a696266110165734bd25ff165e0c
yfuseki <yfuseki@fourmix.co.jp>
2017-09-08 11:47:14 +0900

    503エラー文変更

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -40,7 +40,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">サーバーにアクセスできません.</div>
+                <div class="title">メンテナンス中です。</div>
             </div>
         </div>
     </body>
