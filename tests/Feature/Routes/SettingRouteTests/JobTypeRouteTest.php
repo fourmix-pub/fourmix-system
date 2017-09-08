31d9b9cc8646a58c9cce86d120e3d005b4deb64a
yfuseki <yfuseki@fourmix.co.jp>
2017-09-08 09:58:01 +0900

    テスト修正（デリート）2

--- tests/Feature/Routes/SettingRouteTests/JobTypeRouteTest.php
+++ tests/Feature/Routes/SettingRouteTests/JobTypeRouteTest.php
@@ -115,6 +115,6 @@ class JobTypeRouteTest extends TestCase
         $response = $this->actingAs($this->user)->delete('/settings/job-types/'.$this->jobType->id, ['_token' => csrf_token()]);
         $response->assertStatus(302);
         $response->assertSessionHas('status');
-        $this->assertSoftDeleted('job_types', $this->jobType->id);
+        $this->assertSoftDeleted('job_types', ['id' => $this->jobType->id]);
     }
 }
