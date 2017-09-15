d3747c7b72dddcb6efb776da72420854089b7dad
yfuseki <yfuseki@fourmix.co.jp>
2017-09-15 17:17:28 +0900

    顧客設定 編集修正

--- resources/views/layouts/customers/register-customer-types.blade.php
+++ resources/views/layouts/customers/register-customer-types.blade.php
@@ -3,12 +3,11 @@
         <div class="form-group{{ $errors->has('type_id') ? ' has-error' : '' }}">
             <label class="col-xs-12 col-md-3 col-sm-3 col-lg-3 control-label" for="type_id">
                 顧客種類
-                <span class="text-danger">*</span>
             </label>
             <div class="col-xs-12 col-md-8 col-sm-8 col-lg-8">
                 @foreach(config('system.customer.name') as $key => $value)
                     <label class="radio-inline">
-                        <input type="radio" name="type_id" id="type_id" value="{{ $key }}" @if(($typeId != null ? (int)$typeId : 999) === (int)$key) checked @endif>{{ $value }}
+                        <input type="radio" name="type_id" id="type_id" value="{{ $key }}" @if(($customer ? (int)$customer->type_id : 999) === (int)$key) checked @endif>{{ $value }}
                     </label>
                 @endforeach
                 @include('layouts.common.error-one', ['field' => 'type_id'])
