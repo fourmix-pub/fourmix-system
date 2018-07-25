4db820625db10eb268d238a24b9673b2f213778b
osawanoriko <noosawa@fourmix.co.jp>
2018-07-25 11:17:27 +0900

    安否確認機能　indexファイル追記　ルート追加

--- resources/views/mocks/safety-mails/index.blade.php
+++ resources/views/mocks/safety-mails/index.blade.php
@@ -27,7 +27,38 @@
     {{-- コンテンツ --}}
     <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-
+            <div class="table-responsive">
+                <table class="table table-bordered table-hover">
+                    <thead>
+                        <tr class="active">
+                            <th>日時</th>
+                            <th>タイトル</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>2016.02.01</td>
+                        <td>台風12号　関東上陸</td>
+                        <td></td>
+                    </tr>
+                    </tbody>
+                    <tbody>
+                    <tr>
+                        <td>2013.02.01</td>
+                        <td>大雪による交通機関への影響</td>
+                        <td></td>
+                    </tr>
+                    </tbody>
+                    <tbody>
+                        <tr>
+                            <td>2011.3.11</td>
+                            <td>東北地方太平洋沖地震</td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 
