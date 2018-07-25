efbdf3b20f02cbac582852d02e8dcdd6373d148e
osawanoriko <noosawa@fourmix.co.jp>
2018-07-25 10:50:11 +0900

    安否確認機能　モックファイルを追加

---
+++ resources/views/mocks/safety-mails/index.blade.php
@@ -0,0 +1,34 @@
+
+@extends('layouts.app')
+
+@section('title')
+    test
+@endsection
+
+@php
+    $nav = 'tools';
+@endphp
+
+@section('content')
+
+    {{-- タイトル --}}
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <div class="page-header">
+                <div>
+                    <h2>
+                        安否確認
+                    </h2>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    {{-- コンテンツ --}}
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+
+        </div>
+    </div>
+
+@endsection
