22be21b21797d6ea4d91019d714774db86420705
osawanoriko <noosawa@fourmix.co.jp>
2018-07-26 11:51:44 +0900

    安否確認機能　index.blade修正

--- resources/views/mocks/safety-mails/confirmation.blade.php
+++ resources/views/mocks/safety-mails/confirmation.blade.php
@@ -25,7 +25,7 @@
 
     {{-- コンテンツ --}}
     <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10">
             <div class="panel panel-default">
                 <div class="panel-body">
                     <div class="table-responsive">
@@ -33,22 +33,47 @@
                             <thead>
                                 <tr>
                                     <th>氏名</th>
-                                    <th>安否</th>
+                                    <th></th>
                                     <th>再送信</th>
                                 </tr>
                             </thead>
                             <tbody>
-                            <tr>
-                                <td>大澤乃梨子</td>
-                                <td>
-                                    <span class="label label-success">
-                                         <i class="glyphicon glyphicon-ok-sign" aria-hidden="true"></i> 確認
-                                    </span>
-                                </td>
-                                <td></td>
-                            </tr>
+                                <tr>
+                                    <td>大澤乃梨子</td>
+                                    <td>
+                                        <span class="label label-success">
+                                             <i class="glyphicon glyphicon-ok-sign" aria-hidden="true"></i> 確認
+                                        </span>
+                                    </td>
+                                    <td>
+                                        <div class="checkbox">
+                                            <label>
+                                                <input type="checkbox" id="blankCheckbox" value="option1" aria-label="...">
+                                            </label>
+                                        </div>
+                                    </td>
+                                </tr>
+
+                                <tr>
+                                    <td>大澤乃梨子</td>
+                                    <td>
+                                        <span class="label label-default">
+                                             <i class="glyphicon glyphicon-question-sign" aria-hidden="true"></i> 未確認
+                                        </span>
+                                    </td>
+                                    <td>
+                                        <div class="checkbox">
+                                            <label>
+                                                <input type="checkbox" id="blankCheckbox" value="option1" aria-label="...">
+                                            </label>
+                                        </div>
+                                    </td>
+                                </tr>
                             </tbody>
                         </table>
+                        <button type="button" class="btn btn-success pull-right" style="margin-right: 10px;">
+                            <i class="glyphicon glyphicon-envelope" aria-hidden="true"></i> <span>メール再送信</span>
+                        </button>
                     </div>
                 </div>
             </div>
--- resources/views/mocks/safety-mails/index.blade.php
+++ resources/views/mocks/safety-mails/index.blade.php
@@ -27,12 +27,52 @@
     {{-- コンテンツ --}}
     <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-            <button type="button" class="btn btn-danger pull-right" style="margin-right: 10px;">
+            <button type="button" class="btn btn-success pull-right" style="margin-right: 10px;">
                 <i class="glyphicon glyphicon-envelope" aria-hidden="true"></i> <span>新規メール作成</span>
             </button>
         </div>
     </div>
     <br>
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                                    2012.02.01
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                                    <i class="glyphicon glyphicon-envelope" aria-hidden="true"></i>
+                                    大雪の影響による交通機関の乱れ
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10">
+                                    40%　Confirmed (4/10)
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10">
+                                    <div class="progress">
+                                        <div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
+                                            <span class="sr-only">40% Complete (success)</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
             <div class="panel panel-default">
@@ -44,18 +84,20 @@
                     </div>
                     <div class="row">
                         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                            <i class="glyphicon glyphicon-envelope" aria-hidden="true"></i>
                             大雪の影響による交通機関の乱れ
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                             <div class="progress">
-                                <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
+                                <div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
                                     <span class="sr-only">40% Complete (success)</span>
                                 </div>
                             </div>
                         </div>
                     </div>
+                </div>
                     <div class="row">
                         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                         </div>
