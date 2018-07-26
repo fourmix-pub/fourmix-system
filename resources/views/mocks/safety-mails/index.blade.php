053df03978919a77eea5586a724431a53fd4ad30
osawanoriko <noosawa@fourmix.co.jp>
2018-07-26 12:42:03 +0900

    安否確認機能　index.blade レイアウト修正

--- resources/views/mocks/safety-mails/index.blade.php
+++ resources/views/mocks/safety-mails/index.blade.php
@@ -38,16 +38,18 @@
             <div class="panel panel-default">
                 <div class="panel-body">
                     <div class="row">
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                            2012.02.01
+                        </div>
+                    </div>
+                    <div class="row">
                         <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
                             <div class="row">
                                 <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                                    2012.02.01
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                                    <i class="glyphicon glyphicon-envelope" aria-hidden="true"></i>
-                                    大雪の影響による交通機関の乱れ
+                                    <h4>
+                                        <i class="glyphicon glyphicon-envelope" aria-hidden="true"></i>
+                                        大雪の影響による交通機関の乱れ
+                                    </h4>
                                 </div>
                             </div>
                         </div>
@@ -66,40 +68,6 @@
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-            <div class="panel panel-default">
-                <div class="panel-body">
-                    <div class="row">
-                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                            2012.02.01
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                            <i class="glyphicon glyphicon-envelope" aria-hidden="true"></i>
-                            大雪の影響による交通機関の乱れ
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                            <div class="progress">
-                                <div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
-                                    <span class="sr-only">40% Complete (success)</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                    <div class="row">
-                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                         </div>
                     </div>
                 </div>
