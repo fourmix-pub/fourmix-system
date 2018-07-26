4acc54cafbf118e7a37691dd45735802cefaa96b
osawanoriko <noosawa@fourmix.co.jp>
2018-07-26 15:01:07 +0900

    安否確認機能　index.blade レイアウト修正

--- resources/views/mocks/safety-mails/index.blade.php
+++ resources/views/mocks/safety-mails/index.blade.php
@@ -39,7 +39,7 @@
                 <div class="panel-body">
                     <div class="row">
                         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                            2012.02.01
+                            2017.02.01
                         </div>
                     </div>
                     <div class="row">
@@ -56,7 +56,7 @@
                         <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
                             <div class="row">
                                 <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10">
-                                    40%　Confirmed (4/10)
+                                    <span class="safety-mails-ratio">40</span>%　Confirmed (4/10)
                                 </div>
                             </div>
                             <div class="row">
@@ -64,6 +64,90 @@
                                     <div class="progress">
                                         <div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
                                             <span class="sr-only">40% Complete (success)</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                            2015.05.05
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                                    <h4>
+                                        <i class="glyphicon glyphicon-envelope" aria-hidden="true"></i>
+                                        千葉を震源とする大型地震の発生
+                                    </h4>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10">
+                                    80%　Confirmed (8/10)
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10">
+                                    <div class="progress">
+                                        <div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
+                                            <span class="sr-only">80% Complete (success)</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                            2012.08.23
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                                    <h4>
+                                        <i class="glyphicon glyphicon-envelope" aria-hidden="true"></i>
+                                        台風16号 関東上陸
+                                    </h4>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10">
+                                    100%　Confirmed (10/10)
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10">
+                                    <div class="progress">
+                                        <div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
+                                            <span class="sr-only">100% Complete (success)</span>
                                         </div>
                                     </div>
                                 </div>
--- resources/views/mocks/threads/index.blade.php
+++ resources/views/mocks/threads/index.blade.php
@@ -24,9 +24,42 @@
     </div>
 
     {{-- コンテンツ --}}
+    {{-- 検索ボタン --}}
     <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <button type="button" class="btn btn-primary pull-right" style="margin-right: 5%;" data-toggle="collapse" href="#search">
+                <i class="fa fa-search" aria-hidden="true"></i> <span class="hidden-xs">検索</span>
+            </button>
+        </div>
+    </div>
 
+    {{-- スレッド表示 --}}
+    <div class="row">
+        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-2">
+            追加
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-2">
+            検索
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <div class="row">
+                <div class="col-xs-6 col-sm-6 col-md-6 col-lg-62">
+                    タイトル
+                </div>
+                <div class="col-xs-6 col-sm-6 col-md-6 col-lg-2">
+                    作成者
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-6 col-sm-6 col-md-6 col-lg-2">
+                    更新日時
+                </div>
+                <div class="col-xs-6 col-sm-6 col-md-6 col-lg-2">
+                    作成日時
+                </div>
+            </div>
         </div>
     </div>
 
