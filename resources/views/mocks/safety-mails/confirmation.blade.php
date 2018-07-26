8f4ccd801ee09b1af22b031da45a5f5ac32c06c3
osawanoriko <noosawa@fourmix.co.jp>
2018-07-26 17:52:39 +0900

    安否確認機能　デザイン調整　CSSファイル追記

--- resources/views/mocks/safety-mails/confirmation.blade.php
+++ resources/views/mocks/safety-mails/confirmation.blade.php
@@ -29,7 +29,7 @@
             <div class="panel panel-default">
                 <div class="panel-body">
                     <div class="table-responsive table-confirmation">
-                        <table class="table">
+                        <table class="table mail-confirmation">
                             <thead>
                                 <tr>
                                     <th></th>
@@ -69,9 +69,73 @@
                                         </div>
                                     </td>
                                 </tr>
+
+                                <tr>
+                                    <td>大澤乃梨子</td>
+                                    <td>
+                                        <span class="label label-default">
+                                             <i class="glyphicon glyphicon-question-sign" aria-hidden="true"></i> 未確認
+                                        </span>
+                                    </td>
+                                    <td>
+                                        <div class="checkbox">
+                                            <label>
+                                                <input type="checkbox" id="blankCheckbox" value="option1" aria-label="...">
+                                            </label>
+                                        </div>
+                                    </td>
+                                </tr>
+
+                                <tr>
+                                    <td>大澤乃梨子</td>
+                                    <td>
+                                        <span class="label label-success">
+                                             <i class="glyphicon glyphicon-ok-sign" aria-hidden="true"></i> 確認
+                                        </span>
+                                    </td>
+                                    <td>
+                                        <div class="checkbox">
+                                            <label>
+                                                <input type="checkbox" id="blankCheckbox" value="option1" aria-label="...">
+                                            </label>
+                                        </div>
+                                    </td>
+                                </tr>
+
+                                <tr>
+                                    <td>大澤乃梨子</td>
+                                    <td>
+                                        <span class="label label-success">
+                                             <i class="glyphicon glyphicon-ok-sign" aria-hidden="true"></i> 確認
+                                        </span>
+                                    </td>
+                                    <td>
+                                        <div class="checkbox">
+                                            <label>
+                                                <input type="checkbox" id="blankCheckbox" value="option1" aria-label="...">
+                                            </label>
+                                        </div>
+                                    </td>
+                                </tr>
+
+                                <tr>
+                                    <td>大澤乃梨子</td>
+                                    <td>
+                                        <span class="label label-default">
+                                             <i class="glyphicon glyphicon-question-sign" aria-hidden="true"></i> 未確認
+                                        </span>
+                                    </td>
+                                    <td>
+                                        <div class="checkbox">
+                                            <label>
+                                                <input type="checkbox" id="blankCheckbox" value="option1" aria-label="...">
+                                            </label>
+                                        </div>
+                                    </td>
+                                </tr>
                             </tbody>
                         </table>
-                        <button type="button" class="btn btn-success pull-right" style="margin-right: 10px;">
+                        <button type="button" class="btn btn-success pull-right mail-create-button" style="margin-right: 10px;">
                             <i class="glyphicon glyphicon-envelope" aria-hidden="true"></i> <span>メール再送信</span>
                         </button>
                     </div>
