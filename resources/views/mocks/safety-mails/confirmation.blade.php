4acacbcbb67cdcd36b86bd127818bdba2301a030
osawanoriko <noosawa@fourmix.co.jp>
2018-07-27 13:23:44 +0900

    安否確認機能　確認画面修正前

--- resources/views/mocks/safety-mails/confirmation.blade.php
+++ resources/views/mocks/safety-mails/confirmation.blade.php
@@ -26,61 +26,57 @@
     {{-- コンテンツ --}}
     <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-            <div class="panel panel-default">
-                <div class="panel-body">
-                    <div class="table-responsive table-confirmation">
-                        <table class="table mail-confirmation">
-                            <thead>
-                                <tr>
-                                    <th>氏名</th>
-                                    <th>所属</th>
-                                    <th>安否</th>
-                                    <th>再送信</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>大澤乃梨子</td>
-                                    <td>システムデザイングループ</td>
-                                    <td>
-                                        <span class="label label-success safety-status">
-                                             <i class="glyphicon glyphicon-ok-sign" aria-hidden="true"></i> 確認
-                                        </span>
-                                    </td>
-                                    <td>
-                                        <div class="checkbox">
-                                            <label>
-                                                <input type="checkbox" id="blankCheckbox" value="option1" aria-label="...">
-                                            </label>
-                                        </div>
-                                    </td>
-                                </tr>
-
-                                <tr>
-                                    <td>大澤乃梨子</td>
-                                    <td>システムデザイングループ</td>
-                                    <td>
-                                        <span class="label label-default safety-status">
-                                             <i class="glyphicon glyphicon-question-sign" aria-hidden="true"></i> 未確認
-                                        </span>
-                                    </td>
-                                    <td>
-                                        <div class="checkbox">
-                                            <label>
-                                                <input type="checkbox" id="blankCheckbox" value="option1" aria-label="...">
-                                            </label>
-                                        </div>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <div class="row">
-                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                                <button type="button" class="btn registration-daily pull-right" style="margin-right: 10px;">
-                                    <i class="glyphicon glyphicon-envelope" aria-hidden="true"></i> <span>メール再送信</span>
-                                </button>
+            <div class="table-responsive table-confirmation">
+                <table class="table table-bordered">
+                    <thead>
+                    <tr>
+                        <th>氏名</th>
+                        <th>所属</th>
+                        <th>安否</th>
+                        <th>再送信</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>大澤乃梨子</td>
+                        <td>システムデザイングループ</td>
+                        <td>
+                            <span class="label label-success safety-status">
+                                <i class="glyphicon glyphicon-ok-sign" aria-hidden="true"></i> 確認
+                            </span>
+                        </td>
+                        <td>
+                            <div class="checkbox">
+                                <label>
+                                    <input type="checkbox" id="blankCheckbox" value="option1" aria-label="...">
+                                </label>
                             </div>
-                        </div>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>大澤乃梨子</td>
+                        <td>システムデザイングループ</td>
+                        <td>
+                            <span class="label label-default safety-status">
+                                <i class="glyphicon glyphicon-question-sign" aria-hidden="true"></i> 未確認
+                            </span>
+                        </td>
+                        <td>
+                            <div class="checkbox">
+                                <label>
+                                    <input type="checkbox" id="blankCheckbox" value="option1" aria-label="...">
+                                </label>
+                            </div>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+                <div class="row">
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                        <button type="button" class="btn registration-daily pull-right" style="margin-right: 10px;">
+                            <i class="glyphicon glyphicon-envelope" aria-hidden="true"></i> <span>メール再送信</span>
+                        </button>
                     </div>
                 </div>
             </div>
