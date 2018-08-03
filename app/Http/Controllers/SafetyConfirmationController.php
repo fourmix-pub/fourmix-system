c179a63fa6fb559fd7fbecebb2b8e6a5e70aa63e
osawanoriko <noosawa@fourmix.co.jp>
2018-08-03 10:55:30 +0900

    安否確認　モデル作成

---
+++ app/Http/Controllers/SafetyConfirmationController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\SafetyConfirmation;
+use Illuminate\Http\Request;
+
+class SafetyConfirmationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\SafetyConfirmation  $safetyConfirmation
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SafetyConfirmation $safetyConfirmation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\SafetyConfirmation  $safetyConfirmation
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(SafetyConfirmation $safetyConfirmation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\SafetyConfirmation  $safetyConfirmation
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, SafetyConfirmation $safetyConfirmation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\SafetyConfirmation  $safetyConfirmation
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SafetyConfirmation $safetyConfirmation)
+    {
+        //
+    }
+}
