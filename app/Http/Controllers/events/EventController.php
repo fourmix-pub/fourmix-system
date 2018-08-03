2bcae4a15c423f147891a4211fd46afc743ceb41
nagahoayaka <anagaho@fourmix.co.jp>
2018-08-03 17:39:48 +0900

    エラー修正、migrationファイル追加

---
+++ app/Http/Controllers/events/EventController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Events;
+
+use App\Contracts\Repositories\EventRepositoryContract;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+
+class EventController extends Controller
+{
+    protected $eventRepository;
+    /**
+     * Create a new controller instance.
+     *
+     * @param EventRepositoryContract $eventRepository
+     */
+    public function __construct(EventRepositoryContract $eventRepository)
+    {
+        $this->eventRepository = $eventRepository;
+    }
+    /**
+     * Display a listing of the resource.
+     *一覧表示機能
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('events.events', $this->eventRepository->eventResources());
+    }
+
+
+
+}
---
+++ database/factories/EventFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+use Faker\Factory as Faker;
+
+$faker = Faker::create('ja_JP');
+
+$factory->define(App\Note::class, function()use ($faker) {
+    return [
+        'title' => $faker->text(25),
+        'contents' => $faker->realText(20, 5),
+        'user_id' => 1,
+        'location' => $faker->realText(20, 5),
+    ];
+});
