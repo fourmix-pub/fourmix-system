d7db36ab6c3eacd23dd7e99183d1478708d1ead7
osawanoriko <noosawa@fourmix.co.jp>
2018-08-06 12:22:13 +0900

    安否確認　微調整

--- app/Models/SafetyMail.php
+++ app/Models/SafetyMail.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class SafetyMail extends Model
@@ -24,5 +25,17 @@ class SafetyMail extends Model
     public function user()
     {
         return $this->hasOne(User::class, 'id', 'user_id');
+    }
+
+    /**
+     * 安否確認の割合を求める
+     * @param User $user
+     * @return int
+     */
+    public function confirmationRateRatio(User $user)
+    {
+        $users = $user::table('users')->count();
+        $confirmation = $this->Confirmations()->count();
+        $ratio = $confirmation / $users * 100;
     }
 }
