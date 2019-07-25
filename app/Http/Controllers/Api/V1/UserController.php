42e6e9f9dceedd1a326f82864ef82f41d54b426b
TakagiShun19 <so.dm.bh.sin@i.softbank.jp>
2019-07-25 18:33:23 +0900

    髙木担当分のユーザー・部署リソース

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -2,13 +2,49 @@
 
 namespace App\Http\Controllers\Api\V1;
 
+use App\Http\Resources\V1\UserResource;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class UserController extends Controller
 {
+    /**
+     * ユーザーのプロフィールの取得
+     * @param Request $request
+     * @return UserResource
+     */
+    public function profile(Request $request)
+    {
+        return new UserResource($request->user());
+    }
+
+    /**
+     * 全ユーザーの取得
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     */
     public function index()
     {
-        echo "sdfsadf";
+        return UserResource::collection();
+    }
+
+    /**
+     * ユーザーの更新
+     * @param Request $request
+     * @param User $user
+     * @return UserResource
+     */
+    public  function update(Request $request, User $user) {
+        $this->validate($request, [
+            'name' => 'required|String|min:1|max:50',
+            'email' => 'required|String|min:1',
+        ]);
+
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+
+        $user->update();
+
+        return new UserResource($user);
+
     }
 }
