aade9bc1f5f9f37a733e1aa299a0d95ad36ad1cf
TakagiShun19 <so.dm.bh.sin@i.softbank.jp>
2019-07-26 15:58:29 +0900

    髙木担当分のユーザー・部署のコントローラー

--- routes/api-v1.php
+++ routes/api-v1.php
@@ -8,6 +8,10 @@ use Illuminate\Http\Request;
 |--------------------------------------------------------------------------
 */
 Route::middleware(['auth:api'])->group(function () {
-    //Route::get('test', 'UserController@index');
+    Route::get('/users/profile', 'UserController@profile')->name('profileApi');
+    Route::get('/users', 'UserController@index')->name('usersApi');
+    Route::patch('/users/profile/{user}', 'UserController@update')->name('updateUserApi');
+    Route::get('/users/dailies', 'UserController@myDailies')->name('myDailiesApi');
+
+    Route::get('/departments', 'DepartmentController@index')->name('departmentApi');
 });
-Route::get('test', 'UserController@index');
